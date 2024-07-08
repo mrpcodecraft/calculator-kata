681dfc4d0bbe9567acb7eb44af4ede58b9d06add
Mohit Raj Panday <coder.mrp@gmail.com>
2024-07-08 22:50:37 +0530

    code updates to pass test case no 5

--- src/calculator.ts
+++ src/calculator.ts
@@ -7,7 +7,16 @@ class Calculator {
 
     parseString(str: string): number[] {
         if (!str) return [0];
-        let values = str.split(/[\n,]+/).map(v => parseInt(v) || 0);
+        let values: number[] = [];
+        
+        if (str.startsWith("//")) {
+            const delimiter = str.charAt(2);
+            const rest = str.substring(4);
+            str = rest.split(delimiter).map(v => parseInt(v) || 0).join(",");
+        }
+        
+        values = str.split(/[\n,]+/).map(v => parseInt(v) || 0);
+        
         return values;
     }
 }
