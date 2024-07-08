322dba990ad296f1cd6eeed9ec253973a754c79b
Mohit Raj Panday <coder.mrp@gmail.com>
2024-07-08 22:43:59 +0530

    addition of test case '5) Test for string with custom delimiter and next line tag seperated multiple value'

--- src/calculator.spec.ts
+++ src/calculator.spec.ts
@@ -23,4 +23,8 @@ describe("Test cases for calculator", () => {
     it("4) Test for string with comma and next line tag seperated multiple value", () => {
         expect(add("1\n2,3")).to.equal(6);
     });
+    
+    it("5) Test for string with custom delimiter and next line tag seperated multiple value", () => {
+        expect(add("//;\n1;2;5")).to.equal(8);
+    });
 })
